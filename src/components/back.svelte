22d86d6b4f24a98623465bd64c2ccb1a8a1d48fd
Jack MB <hi@jackmurraybrown.com>
2024-11-20 12:38:28 +0100

    add app navigation and back button component

---
+++ src/components/back.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Icon from "@iconify/svelte";
+
+  function goBack() {
+    window.history.back();
+  }
+</script>
+
+<button onclick={goBack}>
+  <Icon class="h-6 w-6" icon="lucide:arrow-left" />
+  <span class="sr-only">Back</span>
+</button>
--- src/routes/join/+page.svelte
+++ src/routes/join/+page.svelte
@@ -1,6 +1,15 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+
+  function join(event: Event) {
+    event.preventDefault();
+    goto(`/app/events`);
+  }
+</script>
+
 <h1>My Festival</h1>
 
-<form>
+<form onsubmit={join}>
   <input id="name" name="name" type="text" placeholder="Your name" />
   <textarea id="message" name="message" rows="4" placeholder="Your message"
   ></textarea>
