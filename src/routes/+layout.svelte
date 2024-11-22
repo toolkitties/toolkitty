446e39abc0c96bc9ae2a25a098baa6b76324c450
Jack MB <hi@jackmurraybrown.com>
2024-11-22 12:13:23 +0100

    add back button to create and join pages through layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,17 @@
 <script>
   import "../app.css";
+  import Back from "../components/back.svelte";
+  import { page } from "$app/stores";
+
+  let indexPage = $derived($page.url.pathname == "/");
 </script>
 
 <main class="p-8 flex flex-col gap-4 h-dvh">
+  <!-- to do: move to header component -->
+  {#if !indexPage}
+    <div class="fixed top-2 left-2">
+      <Back />
+    </div>
+  {/if}
   <slot />
 </main>
