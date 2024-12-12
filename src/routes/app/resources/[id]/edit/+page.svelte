74e8b3eb32933ecc6e0d96eacd52f46676d8c4db
Charlotte Roe <66831847+ckjr3000@users.noreply.github.com>
2024-12-12 11:57:00 +0000

    Build time picker as select component with hour slots as options

--- src/routes/app/resources/[id]/edit/+page.svelte
+++ src/routes/app/resources/[id]/edit/+page.svelte
@@ -1,1 +1,7 @@
+<script>
+    import CustomCalendar from '../../../../../components/CustomCalendar.svelte';
+</script>
+
 <h1>Edit resource</h1>
+    
+<CustomCalendar canSelectMultiple={false} hasTimePicker={true}/>
