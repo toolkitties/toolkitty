8e8408eeea37d2009a2b748b93890d0cc4620596
Jack MB <hi@jackmurraybrown.com>
2024-12-17 13:13:09 +0100

    add events row component and plcaeholder events for testing

---
+++ src/components/event-row.svelte
@@ -0,0 +1,27 @@
+<script type="ts">
+  // TODO: Define the type of the event prop
+  // @ts-ignore
+  let { event } = $props();
+
+  // TODO: possibly need to convert date to nice format from ISO 8601. TBC.
+</script>
+
+<div class="flex border-black border event-row">
+  <img
+    src={event.image}
+    alt=""
+    class="aspect-square object-cover w-[30%] min-w-32"
+  />
+  <div class="flex flex-col gap-1 p-1">
+    <h3>{event.title}</h3>
+    <span>{event.date}</span>
+    <span>{event.location}</span>
+    <span class="line-clamp-2">{event.description}</span>
+  </div>
+</div>
+
+<style>
+  .event-row:not(:last-child) {
+    border-bottom: 0px;
+  }
+</style>
--- src/routes/app/events/+page.svelte
+++ src/routes/app/events/+page.svelte
@@ -1,4 +1,31 @@
+<script>
+  import EventRow from "../../../components/event-row.svelte";
+
+  let events = [
+    {
+      title: "Event 1",
+      date: "2025-01-01",
+      location: "Location 1",
+      description:
+        "We are convening to discuss potential risks involved in playing the latest version of the game and how to mitigate them. Should be fun plz stop by.",
+      image: "https://placecats.com/louie/300/200",
+    },
+    {
+      title: "Event 2",
+      date: "2025-01-02",
+      location: "Location 2",
+      description:
+        "We are convening to discuss potential risks involved in playing the latest version of the game and how to mitigate them. Should be fun plz stop by.",
+      image: "https://placecats.com/bella/300/200",
+    },
+  ];
+</script>
+
 <h1>Events page</h1>
 
 <a href="/app/events/1">Event link </a>
 <a href="/app/events/create">Create event</a>
+
+{#each events as event}
+  <EventRow {event} />
+{/each}
