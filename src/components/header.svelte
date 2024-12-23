a5746698c718696d583f1548647c629bf9c8b166
Jack MB <hi@jackmurraybrown.com>
2024-12-23 11:26:09 +0000

    move header out of layout and into its own component

---
+++ src/components/header.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Back from "./back.svelte";
+
+  let { title = "Default Title" } = $props();
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
+
+<div class="fixed top-0 left-0 p-2 right-2 flex items-center w-full bg-white">
+  <Back />
+  <h1 class="grow text-center">{title}</h1>
+  <div class="w-[42px]"></div>
+</div>
