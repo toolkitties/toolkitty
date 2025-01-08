543d556c977cb90e8465d5da33e7f0b164b7a409
Sam Andreae <contact@samandreae.com>
2025-01-08 11:47:04 +0000

    Basic NodeActor containing Network instance

--- src-tauri/src/node/actor.rs
+++ src-tauri/src/node/actor.rs
@@ -1,7 +1,21 @@
-pub struct NodeActor {}
+use anyhow::Result;
+use p2panda_net::{Network, TopicId};
+use p2panda_sync::TopicQuery;
+use tokio::sync::{mpsc, oneshot};
 
-impl NodeActor {
-    pub fn new() -> Self {
-        Self {}
+pub enum ToNodeActor {
+    Publish { reply: oneshot::Sender<Result<()>> },
+    Subscribe { reply: oneshot::Sender<Result<()>> },
+    Shutdown { reply: oneshot::Sender<()> },
+}
+
+pub struct NodeActor<T> {
+    network: Network<T>,
+    inbox: mpsc::Receiver<ToNodeActor>,
+}
+
+impl<T: TopicId + TopicQuery> NodeActor<T> {
+    pub fn new(network: Network<T>, inbox: mpsc::Receiver<ToNodeActor>) -> Self {
+        Self { network, inbox }
     }
 }
