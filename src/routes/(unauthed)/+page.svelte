4314580add25311f83204df54c33b4bacf865d2d
Jack MB <hi@jackmurraybrown.com>
2025-01-10 17:32:19 +0100

    ui states for finding calendar

--- src/routes/(unauthed)/+page.svelte
+++ src/routes/(unauthed)/+page.svelte
@@ -6,39 +6,27 @@
   let value: string[] | undefined = [];
 
   let unlocked = true;
-  let progress : "dormant" | "pending" | "timeout" = "dormant";
+  let progress: "dormant" | "pending" = "dormant";
+  let timedOut: boolean = false;
   let pinInputType: "text" | "password" = "password";
   $: pinInputType = unlocked ? "text" : "password";
 
   async function join(event: Event) {
     event.preventDefault();
 
+    if (!value) return;
+
     let calendarId;
 
     try {
       progress = "pending";
-      calendarId = await resolveInviteCode("5a7b");
+      calendarId = await resolveInviteCode(value.join(""));
     } catch (err) {
-      progress = "timeout";
+      timedOut = true;
+      progress = "dormant";
       console.error(err);
       return;
     }
-
-    //     // @TODO: Just doing all this here for testing purposes, move somewhere
-    //     // sensible later, of course.
-    //     const calendarId = "5a7bc8522433759260bdcb77648890b5da10297ed477776611c3c5f83342b025";
-    //
-    //     await invoke("select_calendar", { calendarId });
-    //
-    //     // Just some app data.
-    //     const payload = {
-    //       type: "EventCreated",
-    //       data: { title: "My Cool Event" },
-    //     };
-    //
-    //     // Publish the app event via the publish command.
-    //     console.log(`publish application data: `, payload);
-    //     await invoke("publish", { payload, calendarId });
 
     goto(`/join?code=${calendarId}`);
   }
@@ -47,39 +35,47 @@
 <h1 class="text-3xl text-center">Toolkitty 🐈</h1>
 
 <form class="flex flex-col gap-4 grow justify-center mx-auto" onsubmit={join}>
-  <PinInput.Root
-    bind:value
-    class="flex items-center gap-2"
-    type={pinInputType}
-    placeholder="0"
-  >
-    <PinInput.Input
-      class="flex h-input w-10 select-none rounded-input border border-foreground bg-background px-2 py-3 text-center font-alt text-[17px] tracking-[0.01em] text-foreground placeholder-shown:border-border-input focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover"
-    />
-    <PinInput.Input
-      class="flex h-input w-10 select-none rounded-input border border-foreground bg-background px-2 py-3 text-center font-alt text-[17px] tracking-[0.01em] text-foreground placeholder-shown:border-border-input focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover"
-    />
-    <PinInput.Input
-      class="flex h-input w-10 select-none rounded-input border border-foreground bg-background px-2 py-3 text-center font-alt text-[17px] tracking-[0.01em] text-foreground placeholder-shown:border-border-input focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover"
-    />
-    <PinInput.Input
-      class="flex h-input w-10 select-none rounded-input border border-foreground bg-background px-2 py-3 text-center font-alt text-[17px] tracking-[0.01em] text-foreground placeholder-shown:border-border-input focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover"
-    />
-    <PinInput.HiddenInput />
-    <Toggle.Root
-      aria-label="toggle code visibility"
-      class="inline-flex size-10 items-center justify-center rounded-[9px] text-foreground/40 transition-all hover:bg-muted active:scale-98 active:bg-dark-10 active:data-[state=on]:bg-dark-10"
-      bind:pressed={unlocked}
+  {#if progress == "dormant"}
+    <PinInput.Root
+      bind:value
+      class="flex items-center gap-2"
+      type={pinInputType}
+      placeholder="0"
     >
-      {#if unlocked}
-        <span>Hide</span>
-      {:else}
-        <span>Show</span>
-      {/if}
-    </Toggle.Root>
-  </PinInput.Root>
-  <button class="border border-black rounded p-4" type="submit">Join</button>
+      <PinInput.Input
+        class="flex h-input w-10 select-none rounded-input border border-foreground bg-background px-2 py-3 text-center font-alt text-[17px] tracking-[0.01em] text-foreground placeholder-shown:border-border-input focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover"
+      />
+      <PinInput.Input
+        class="flex h-input w-10 select-none rounded-input border border-foreground bg-background px-2 py-3 text-center font-alt text-[17px] tracking-[0.01em] text-foreground placeholder-shown:border-border-input focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover"
+      />
+      <PinInput.Input
+        class="flex h-input w-10 select-none rounded-input border border-foreground bg-background px-2 py-3 text-center font-alt text-[17px] tracking-[0.01em] text-foreground placeholder-shown:border-border-input focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover"
+      />
+      <PinInput.Input
+        class="flex h-input w-10 select-none rounded-input border border-foreground bg-background px-2 py-3 text-center font-alt text-[17px] tracking-[0.01em] text-foreground placeholder-shown:border-border-input focus-within:border-border-input-hover focus-within:shadow-date-field-focus hover:border-border-input-hover"
+      />
+      <PinInput.HiddenInput />
+      <Toggle.Root
+        aria-label="toggle code visibility"
+        class="inline-flex size-10 items-center justify-center rounded-[9px] text-foreground/40 transition-all hover:bg-muted active:scale-98 active:bg-dark-10 active:data-[state=on]:bg-dark-10"
+        bind:pressed={unlocked}
+      >
+        {#if unlocked}
+          <span>Hide</span>
+        {:else}
+          <span>Show</span>
+        {/if}
+      </Toggle.Root>
+    </PinInput.Root>
+    <button class="border border-black rounded p-4" type="submit">Join</button>
+  {:else if progress == "pending"}
+    <p>Searching for calendar</p>
+  {/if}
 </form>
+
+{#if timedOut}
+  <p>Calendar not found</p>
+{/if}
 <a
   href="/create"
   class="border border-black rounded p-4 text-center"
