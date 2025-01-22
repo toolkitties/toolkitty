8b41acde585814c6f8ebc7710758977239fb22eb
adz <x12@adz.garden>
2025-01-22 14:57:48 +0100

    Clean up onMount logic a little, add comment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,16 @@
   import { init } from "$lib/channel";
   import { onMount } from "svelte";
 
-  console.log("yo");
   onMount(() => {
+    // Hacky workaround to only call "init" once in a Svelte HMR life-cycle.
+    //
+    // @TODO(adz): This might need some more investigation as it currently
+    // breaks when an error was thrown. After fixing that error, HMR re-loads
+    // fully and calls "init" again.
     if (!("isInit" in window)) {
       init();
+
+      // @ts-ignore
       window.isInit = true;
     }
   });
