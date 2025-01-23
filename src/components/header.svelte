d73645d8748e466369ed53b29fcec29c49fce683
Jack MB <hi@jackmurraybrown.com>
2025-01-23 17:50:14 +0100

    add some styling to header

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -13,10 +13,15 @@
   <title>{title}</title>
 </svelte:head>
 
-<div class="fixed top-0 left-0 p-2 right-2 flex items-center w-full bg-white">
-  <Back />
-  <h1 class="grow text-center">{title}</h1>
-  {#if $activeCalendarName}
-    <div class="w-[42px]">{$activeCalendarName}</div>
-  {/if}
+<div class="fixed top-0 left-0 p-2 right-2 w-full bg-white space-y-1.5">
+  <div class="flex justify-between border border-black rounded-full px-2">
+    <h1>{title}</h1>
+    {#if $activeCalendarName}
+      <div class="w-[42px]">{$activeCalendarName}</div>
+    {/if}
+  </div>
+  <div>
+    <Back />
+  </div>
 </div>
+
