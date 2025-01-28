e67baa8353737e422c81243e58b0cd363bec8341
Jack MB <hi@jackmurraybrown.com>
2025-01-28 16:57:26 +0100

    add new files to index

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,3 +1,6 @@
 export * as calendars from "./calendars";
 export * as events from "./events";
 export * as inviteCodes from "./inviteCodes";
+export * as spaces from "./spaces";
+export * as resources from "./resources";
+export * as requests from "./requests";
