1886cdb18125d097467ce0f49957f081ff3d423b
Charlotte Roe <66831847+ckjr3000@users.noreply.github.com>
2025-02-07 10:45:04 +0000

    Create space-row component

---
+++ src/components/space-row.svelte
@@ -0,0 +1,22 @@
+<script type="ts">
+  let { space } = $props();
+</script>
+
+<a href={`/app/spaces/${space.id}`} class="flex border-black border space-row">
+  <img
+    src={space.image}
+    alt=""
+    class="aspect-square object-cover w-[30%] min-w-32 max-w-72 grow"
+  />
+  <div class="flex flex-col gap-1 p-2">
+    <h3>{space.name}</h3>
+    <span>{space.address}</span>
+    <span>{space.description}</span>
+  </div>
+</a>
+
+<style>
+  .space-row:not(:last-child) {
+    border-bottom: 0px;
+  }
+</style>
--- src/routes/app/spaces/+page.svelte
+++ src/routes/app/spaces/+page.svelte
@@ -1,1 +1,14 @@
-<h1>Spaces page</h1>
+<script>
+</script>
+
+<p>
+  Below is a list of all the venues that are available to host events, with time
+  slots that you can book. If you have organised a place to hold your event
+  yourself, you can register a new venue and even make your space available to
+  other Antiuni organisers.
+</p>
+<p>
+  ❗Deleting your place will cause all events that have chosen your place to
+  disappear❗(please give us or the organisers of the event good warning if you
+  wish to do so)
+</p>
