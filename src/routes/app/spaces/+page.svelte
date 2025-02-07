cb24c72924b4e0e03b3b3fb5599173278e94ef0d
Charlotte Roe <66831847+ckjr3000@users.noreply.github.com>
2025-02-07 10:54:32 +0000

    Build 'spaces' page

--- src/routes/app/spaces/+page.svelte
+++ src/routes/app/spaces/+page.svelte
@@ -1,6 +1,21 @@
-<script>
+<script lang="ts">
+  import { onMount } from "svelte";
+  import SpaceRow from "../../../components/space-row.svelte";
+  import { findMany as findSpaces } from "../../../lib/api/spaces";
+
+  let spaces: Space[] = [];
+
+  onMount(async () => {
+    try {
+      const fetchedSpaces = await findSpaces();
+      spaces = [...fetchedSpaces];
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  });
 </script>
 
+<!--@TODO - replace with 'space page text' data when set up in db & api -->
 <p>
   Below is a list of all the venues that are available to host events, with time
   slots that you can book. If you have organised a place to hold your event
@@ -12,3 +27,7 @@
   disappear❗(please give us or the organisers of the event good warning if you
   wish to do so)
 </p>
+
+{#each spaces as space}
+  <SpaceRow {space} />
+{/each}
