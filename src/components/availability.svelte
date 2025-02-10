e8aa1332cfafe6f29b6d9029e9b1900eb7ed7d5f
Jack MB <hi@jackmurraybrown.com>
2025-02-10 22:03:39 +0100

    add availability viewer component

---
+++ src/components/availability.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+  // these would be our props when we make this into a reusable component.
+  // let { availability, booked } = $props();
+  // for now placing data below so its easier to understand
+
+  const availability = {
+    start: new Date("2025-02-10T10:00Z"),
+    end: new Date("2025-02-10T23:00Z"),
+  };
+
+  const booked = [
+    {
+      start: new Date("2025-02-10T11:00Z"),
+      end: new Date("2025-02-10T12:00Z"),
+    },
+    {
+      start: new Date("2025-02-10T13:00Z"),
+      end: new Date("2025-02-10T15:00Z"),
+    },
+    {
+      start: new Date("2025-02-10T20:30Z"),
+      end: new Date("2025-02-10T22:00Z"),
+    },
+  ];
+
+  /**
+   * Calculate the difference in hours between two dates
+   * */
+  function getHoursDifference(start: Date, end: Date): number {
+    const diffMillis = end.getTime() - start.getTime();
+    return diffMillis / (1000 * 60 * 60); // Convert milliseconds to hours
+  }
+
+  /**
+   * Convert booked TimeSpan into slots that are a percentage
+   * of the total availability for the day, this allows us to
+   * then display them as absolutely positioned elements with
+   * start being % from top and length being % height.
+   */
+  function getBookedSlots() {
+    const totalBookableTime = getHoursDifference(
+      availability.start,
+      availability.end,
+    );
+    return booked.map((booking) => {
+      const start =
+        (getHoursDifference(availability.start, booking.start) /
+          totalBookableTime) *
+        100;
+      const length =
+        (getHoursDifference(booking.start, booking.end) / totalBookableTime) *
+        100;
+      return { start, length };
+    });
+  }
+
+  /**
+   * Get the availability split into hours
+   */
+  function getHours(): string[] {
+    const hours: string[] = [];
+    let current = availability.start;
+
+    while (current < availability.end) {
+      hours.push(current.getUTCHours().toString() + ":00");
+      current.setUTCHours(current.getUTCHours() + 1);
+    }
+
+    return hours;
+  }
+
+  const bookedSlots = getBookedSlots();
+  const hours = getHours();
+</script>
+
+<div class="max-h-[400px] overflow-auto">
+  <div class="flex">
+    <!-- availability as hours -->
+    <ul class="divide-black-200 divide-y-2">
+      {#each hours as hour}
+        <li class="h-12">{hour}</li>
+      {/each}
+    </ul>
+    <!-- bookings -->
+    <div class="grow relative">
+      {#each bookedSlots as block}
+        <div
+          class="bg-red-light absolute w-full"
+          style="top: {block.start}%; height: {block.length}%"
+        ></div>
+      {/each}
+    </div>
+  </div>
+</div>
