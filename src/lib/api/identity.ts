372029c2b55a0bf2640d00759eda131efeffc8c9
Sam Andreae <contact@samandreae.com>
2025-02-10 16:05:38 +0000

    Add TODO

--- src/lib/api/identity.ts
+++ src/lib/api/identity.ts
@@ -1,5 +1,6 @@
 import { invoke } from "@tauri-apps/api/core";
 
 export async function publicKey(): Promise<PublicKey> {
+  // @TODO: Would be good to cache this somewhere probably.
   return await invoke("public_key");
 }
