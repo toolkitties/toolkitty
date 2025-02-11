41009d00f0e57ce4c57c53f2af4fadfeea1fea3a
Sam Andreae <contact@samandreae.com>
2025-02-11 20:37:51 +0000

    fmt

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,8 +8,7 @@ use tauri::Builder;
 
 use crate::rpc::{
     ack, add_calendar_author, create_calendar, init, public_key, publish_calendar_event,
-    publish_to_calendar_inbox, publish_to_invite_code_overlay, select_calendar,
-    subscribe,
+    publish_to_calendar_inbox, publish_to_invite_code_overlay, select_calendar, subscribe,
 };
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
