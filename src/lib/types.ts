a5b430e5233e9e63e190dcd2acf4123af602a258
Sam Andreae <contact@samandreae.com>
2025-02-13 08:54:04 +0000

    Change fields of CalendarAccessAccepted to contain requestId

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -329,8 +329,7 @@ type CalendarAccessRequested = {
 type CalendarAccessAccepted = {
   type: "calendar_access_accepted";
   data: {
-    publicKey: PublicKey,
-    calendarId: Hash;
+    requestId: Hash;
   };
 };
 
