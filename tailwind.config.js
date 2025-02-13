c576756d41b74cf02348fe9a74ad9f4377a3ff8d
Jack MB <hi@jackmurraybrown.com>
2025-02-13 15:27:52 +0100

    move theme colours to css and remove tailwind themer plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,35 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
-      // Constant colours that don't change between themes
       colors: {
-        'red': '#FF9A9C',
-        'purple': {
-          'default': '#C398FF',
+        // Theme dependent colours declared in app.css
+        bg: 'var(--bg-color)',
+        'bg-secondary': 'var(--bg-secondary-color)',
+        primary: 'var(--primary-color)',
+        physical: 'var(--physical-color)',
+        geo: 'var(--geo-color)',
+        virtual: 'var(--virtual-color)',
+        text: 'var(--text-color)',
+        // Constant colours that don't change between themes
+        red: {
+          light: '#FF9A9C'
+        },
+        purple: {
+          default: '#C398FF',
           'very-light': '#E6DBF5'
         },
-        'grey': {
-          'light': '#E1E1E1',
-          'dark': '#8C8C8C',
-          'darker': '#474747'
+        grey: {
+          light: '#E1E1E1',
+          dark: '#8C8C8C',
+          darker: '#474747'
         },
-        'brown': '#322113',
-        'black': '#020202',
-        'pink': {
-          'select': '#FF00E5'
-        }
+        brown: {
+          dark: '#322113'
+        },
+        black: '#020202',
+        pink: {
+          hot: '#FF00E5'
+        },
       },
       fontFamily: {
         sans: ['Atkinson-Hyperlegible', 'sans-serif'],
@@ -27,45 +39,11 @@ export default {
       },
       boxShadow: {
         'default': '-4px 4px 10px rgba(0, 0, 0, 0.25);'
-      }
-
+      },
+      backgroundImage: (theme) => ({
+        'gradient-attention': `linear-gradient(90deg, ${theme('colors.primary')} 24%, ${theme('colors.red.light')} 100%)`,
+      }),
     },
   },
-  plugins: [
-    require('tailwindcss-themer')({
-      defaultTheme: {
-        extend: {
-          /**
-           * Colours for default theme
-           */
-          colors: {
-            'green': '#B4FFB2',
-            'pink': '#FF00E5',
-            'blue': '#FFDFFA',
-            'yellow': '#FDFFB4',
-            'grey': '#C2C2C2',
-          }
-        }
-      },
-      themes: [
-        {
-          name: 'dark',
-          extend: {
-            /**
-             * Colours from dark theme. visually these  might look different 
-             * from the colour mentionedbut we call them the same thing so we
-             * can swap them around automatically
-             */
-            colors: {
-              'green': '#00000',
-              'pink': '#051F07',
-              'blue': '#010143',
-              'yellow': '#3B0346',
-              'grey': '#FFFFFF',
-            }
-          }
-        }
-      ]
-    })
-  ],
+  plugins: [],
 }
