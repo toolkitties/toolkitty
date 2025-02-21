0ff2a6881ea71710acece3d93c415c38bf4cbf1d
Charlotte Roe <66831847+ckjr3000@users.noreply.github.com>
2025-02-21 11:16:02 +0000

    Remove old imports of CustomCalendar

--- src/routes/app/home/+page.svelte
+++ src/routes/app/home/+page.svelte
@@ -1,52 +1,43 @@
 <script lang="ts">
-import type { DateValue } from "@internationalized/date";
-import { CalendarDate } from "@internationalized/date";
-import CustomCalendar from '../../../components/CustomCalendar.svelte';
+  import type { DateValue } from "@internationalized/date";
+  import { CalendarDate } from "@internationalized/date";
 
-// placeholder data
-const festivalDates: DateValue[] = [
+  // placeholder data
+  const festivalDates: DateValue[] = [
     new CalendarDate(2024, 12, 11),
     new CalendarDate(2024, 12, 12),
     new CalendarDate(2024, 12, 13),
     new CalendarDate(2024, 12, 14),
     new CalendarDate(2024, 12, 15),
     new CalendarDate(2024, 12, 16),
-];
+  ];
 
-const eventsCount: Object[] = [
+  const eventsCount: Object[] = [
     {
-        date: new CalendarDate(2024, 12, 11),
-        numberOfEvents: 5
+      date: new CalendarDate(2024, 12, 11),
+      numberOfEvents: 5,
     },
     {
-        date: new CalendarDate(2024, 12, 12),
-        numberOfEvents: 3
+      date: new CalendarDate(2024, 12, 12),
+      numberOfEvents: 3,
     },
     {
-        date: new CalendarDate(2024, 12, 13),
-        numberOfEvents: 2
+      date: new CalendarDate(2024, 12, 13),
+      numberOfEvents: 2,
     },
     {
-        date: new CalendarDate(2024, 12, 14),
-        numberOfEvents: 4
+      date: new CalendarDate(2024, 12, 14),
+      numberOfEvents: 4,
     },
     {
-        date: new CalendarDate(2024, 12, 15),
-        numberOfEvents: 1
+      date: new CalendarDate(2024, 12, 15),
+      numberOfEvents: 1,
     },
     {
-        date: new CalendarDate(2024, 12, 16),
-        numberOfEvents: 0
+      date: new CalendarDate(2024, 12, 16),
+      numberOfEvents: 0,
     },
-]
-
+  ];
 </script>
-<h1>My Festival</h1>
 
-<CustomCalendar 
-    use={"festival overview"}
-    canSelectMultiple={false} 
-    hasTimePicker={false} 
-    festivalDates={festivalDates} 
-    eventsCount={eventsCount}
-/>
+<h1>My Festival</h1>
