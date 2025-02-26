3d7a0077e0314a7b19d69e419da66800a977f046
Charlotte Roe <66831847+ckjr3000@users.noreply.github.com>
2025-02-26 09:46:07 +0000

    Create handleCreateSpace method

--- src/lib/components/SpaceForm.svelte
+++ src/lib/components/SpaceForm.svelte
@@ -1,28 +1,41 @@
-<script>
-  import AvailabilitySetter from "./AvailabilitySetter.svelte";
+<script lang="ts">
+  import AvailabilitySetter from "$lib/components/AvailabilitySetter.svelte";
   let { formType } = $props();
-  console.log(formType);
+  import { getActiveCalendarId } from "$lib/api/calendars";
+  import { create } from "$lib/api/spaces";
+
+  function handleCreateSpace(e: Event) {
+    e.preventDefault();
+
+    const calendarId = getActiveCalendarId();
+
+    const form = e.currentTarget as HTMLFormElement;
+    console.log(form);
+    const formData = new FormData(form);
+    console.log(formData);
+    const fields = {};
+  }
 </script>
 
-<form>
+<form onsubmit={handleCreateSpace}>
   <fieldset>
     <label for="physical">Physical Location</label>
-    <input type="radio" name="space-type" value="physical" />
+    <input type="radio" name="space-type" value="physical" checked />
     <label for="gps">GPS coordinates</label>
     <input type="radio" name="space-type" value="gps" />
     <label for="virtual">Virtual Space</label>
     <input type="radio" name="space-type" value="virtual" />
   </fieldset>
   <label for="address">Address*</label>
-  <input type="text" name="address" />
+  <input type="text" name="address" required />
   <label for="capacity">Capacity*</label>
-  <input type="text" name="capacity" />
+  <input type="text" name="capacity" required />
   <label for="accessibility-details">Accessibility Details*</label>
-  <textarea name="accessibility-details"></textarea>
+  <textarea name="accessibility-details" required></textarea>
   <label for="space-description">Space Description*</label>
-  <textarea name="space-description"></textarea>
+  <textarea name="space-description" required></textarea>
   <label for="contact-details">Contact Details*</label>
-  <input type="text" name="contact-details" />
+  <input type="text" name="contact-details" required />
   <p>🔗 Useful link (website, social media)</p>
   <div class="flex flex-row">
     <div>
