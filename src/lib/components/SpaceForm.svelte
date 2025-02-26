6de96eb965a821895718ca6ff16aeb2ef049f22e
Charlotte Roe <66831847+ckjr3000@users.noreply.github.com>
2025-02-26 12:35:51 +0000

    Have adress fields in space form changing depending on currently selected space type

--- src/lib/components/SpaceForm.svelte
+++ src/lib/components/SpaceForm.svelte
@@ -31,22 +31,66 @@
       console.log(`${key}:`, value);
     }
   }
+
+  let selectedSpaceType = $state("physical");
 </script>
 
 <form onsubmit={handleCreateSpace}>
   <fieldset>
     <label for="physical">Physical Location</label>
-    <input type="radio" name="space-type" value="physical" checked />
+    <input
+      type="radio"
+      name="space-type"
+      value="physical"
+      bind:group={selectedSpaceType}
+      checked
+    />
     <label for="gps">GPS coordinates</label>
-    <input type="radio" name="space-type" value="gps" />
+    <input
+      type="radio"
+      name="space-type"
+      value="gps"
+      bind:group={selectedSpaceType}
+    />
     <label for="virtual">Virtual Space</label>
-    <input type="radio" name="space-type" value="virtual" />
+    <input
+      type="radio"
+      name="space-type"
+      value="virtual"
+      bind:group={selectedSpaceType}
+    />
   </fieldset>
 
   <label for="space-name">Space Name*</label>
   <input type="text" name="space-name" required />
-  <label for="address">Address*</label>
-  <input type="text" name="address" required />
+  {#if selectedSpaceType === "physical"}
+    <fieldset>
+      <legend>Address</legend>
+      <label for="address-street">Street</label>
+      <input type="text" name="address-street" required />
+      <label for="address-city">City</label>
+      <input type="text" name="address-city" required />
+      <label for="address-state">State</label>
+      <input type="text" name="address-state" required />
+      <label for="address-zip">Zip</label>
+      <input type="text" name="address-zip" required />
+      <label for="address-country">Country</label>
+      <input type="text" name="address-country" required />
+    </fieldset>
+  {/if}
+  {#if selectedSpaceType === "gps"}
+    <fieldset>
+      <legend>Co-ordinates</legend>
+      <label for="address-lat">Latitude*</label>
+      <input type="number" name="address-lat" required />
+      <label for="address-lon">Longitude*</label>
+      <input type="number" name="address-lon" required />
+    </fieldset>
+  {/if}
+  {#if selectedSpaceType === "virtual"}
+    <label for="address-virtual">Link to virtual space*</label>
+    <input type="text" name="address-virtual" required />
+  {/if}
   <label for="capacity">Capacity*</label>
   <input type="number" name="capacity" required />
   <label for="accessibility-details">Accessibility Details*</label>
@@ -56,17 +100,19 @@
   <label for="contact-details">Contact Details*</label>
   <input type="text" name="contact-details" required />
 
-  <p>🔗 Useful link (website, social media)</p>
-  <div class="flex flex-row">
-    <div>
-      <label for="space-link-text">Link Title</label>
-      <input type="text" name="space-link-text" />
+  <fieldset>
+    <legend>🔗 Useful link (website, social media)</legend>
+    <div class="flex flex-row">
+      <div>
+        <label for="space-link-text">Link Title</label>
+        <input type="text" name="space-link-text" />
+      </div>
+      <div>
+        <label for="space-link-url">URL</label>
+        <input type="url" name="space-link-url" />
+      </div>
     </div>
-    <div>
-      <label for="space-link-url">URL</label>
-      <input type="url" name="space-link-url" />
-    </div>
-  </div>
+  </fieldset>
 
   <label for="space-message">Your message to anyone booking this space</label>
   <input
