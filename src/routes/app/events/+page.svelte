32be3078b07750c844e5bdac15a7f5c0a43242ef
Charlotte Roe <66831847+ckjr3000@users.noreply.github.com>
2025-02-27 08:14:58 +0000

    Fetch events to events home from API

--- src/routes/app/events/+page.svelte
+++ src/routes/app/events/+page.svelte
@@ -1,31 +1,24 @@
-<script>
+<script lang="ts">
+  import { onMount } from "svelte";
   import EventRow from "$lib/components/EventRow.svelte";
+  import { findMany } from "$lib/api/events";
 
-  let events = [
-    {
-      id: 1,
-      title: "Event 1",
-      date: "Tuesday 19.09.25",
-      time: "5:00pm - 7:30pm",
-      location: "Location 1",
-      image: "https://placecats.com/louie/300/200",
-      tags: ["tag 1", "tag 2", "tag 3"],
-    },
-    {
-      id: 2,
-      title: "Event 2",
-      date: "Tuesday 19.09.25",
-      time: "5:00pm - 7:30pm",
-      location: "Location 2",
-      image: "https://placecats.com/bella/300/200",
-      tags: ["tag 1", "tag 2", "tag 3"],
-    },
-  ];
+  let events: CalendarEvent[] = [];
+
+  onMount(async () => {
+    try {
+      const fetchedEvents = await findMany();
+      events = [...fetchedEvents];
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  });
 </script>
 
+<br />
+<br />
+<br />
 <h1 class="font-pixel">Events page</h1>
-
-<a href="/app/events/1">Event link </a>
 <a href="/app/events/create">Create event</a>
 
 {#each events as event}
