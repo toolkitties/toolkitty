fa5ca172e84670136eefe9b14c75f215abaf33bb
Charlotte Roe <66831847+ckjr3000@users.noreply.github.com>
2025-02-27 09:08:02 +0000

    Plug spaces and resource homes into API

--- src/routes/app/resources/+page.svelte
+++ src/routes/app/resources/+page.svelte
@@ -1,1 +1,38 @@
-<h1>Resources</h1>
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { findMany } from "$lib/api/resources";
+
+  let resources: Resource[] = [];
+
+  onMount(async () => {
+    try {
+      const fetchedresources = await findMany();
+      resources = [...fetchedresources];
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  });
+</script>
+
+<br />
+<br />
+<br />
+<h1 class="font-pixel">Spaces</h1>
+<a href="/app/resources/create">Create resource</a>
+
+{#each resources as resource}
+  <a
+    href={`/app/spaces/${resource.id}`}
+    class="flex border-black border event-row"
+  >
+    <img
+      src={resource.images[0]}
+      alt=""
+      class="aspect-square object-cover w-[30%] min-w-32 max-w-72 grow"
+    />
+    <div class="flex flex-col gap-1 p-2">
+      <h3>{resource.name}</h3>
+      <p>{resource.description}</p>
+    </div>
+  </a>
+{/each}
--- src/routes/app/spaces/+page.svelte
+++ src/routes/app/spaces/+page.svelte
@@ -1,1 +1,39 @@
-<h1>Spaces page</h1>
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { findMany } from "$lib/api/spaces";
+
+  let spaces: Space[] = [];
+
+  onMount(async () => {
+    try {
+      const fetchedSpaces = await findMany();
+      spaces = [...fetchedSpaces];
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  });
+</script>
+
+<br />
+<br />
+<br />
+<h1 class="font-pixel">Spaces</h1>
+<a href="/app/spaces/create">Create space</a>
+
+{#each spaces as space}
+  <a
+    href={`/app/spaces/${space.id}`}
+    class="flex border-black border event-row"
+  >
+    <img
+      src={space.images[0]}
+      alt=""
+      class="aspect-square object-cover w-[30%] min-w-32 max-w-72 grow"
+    />
+    <div class="flex flex-col gap-1 p-2">
+      <h3>{space.name}</h3>
+      <span>📍 {space.location}</span>
+      <p>{space.description}</p>
+    </div>
+  </a>
+{/each}
