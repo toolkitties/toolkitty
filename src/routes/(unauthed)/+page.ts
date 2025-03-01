c373539ee5c2755d89ea9d134b67082c76fb023f
Jack MB <hi@jackmurraybrown.com>
2025-03-01 16:08:31 +0100

    add todo and go to active calendar if set

--- src/routes/(unauthed)/+page.ts
+++ src/routes/(unauthed)/+page.ts
@@ -5,9 +5,11 @@ import { goto } from '$app/navigation';
 export const load: PageLoad = async () => {
   let activeCalendarId = await calendars.getActiveCalendarId();
 
-  // if (activeCalendarId) {
-  //   goto("/app/events")
-  // }
+  // TODO: check if user has access to active calendar before going to the calendar
+  // If access is pending then go to join/pending page.
+  if (activeCalendarId) {
+    goto("/app/events")
+  }
 
   return {
     title: 'welcome'
