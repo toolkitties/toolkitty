4b3b5c8de8204edec486119ee01fbbd389a2b4fa
Jack MB <hi@jackmurraybrown.com>
2025-03-05 14:45:36 +0100

    fix bugs in /events, /spaces, /resources where calendar id was not passed to findMany method

--- src/routes/app/events/+page.ts
+++ src/routes/app/events/+page.ts
@@ -3,7 +3,7 @@ import { calendars, events } from "$lib/api";
 
 export const load: PageLoad = async () => {
   const activeCalendarId = await calendars.getActiveCalendarId();
-  const eventsList = await events.findMany();
+  const eventsList = await events.findMany(activeCalendarId!);
 
   return { title: "home", activeCalendarId, eventsList };
 };
