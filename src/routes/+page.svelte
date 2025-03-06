4d3f0a22c43621d4705649694d6c74efcd675d7c
a.v <hi@jackmurraybrown.com>
2025-03-06 16:31:22 +0100

    Calendar selector component and app loading screen (#182)
    * add CalendarSelector component and initial loading screen for app
    
    * fix type for interval
    
    * fix selector styling and icon default prop bug
    
    * fix url on join function
    
    * add todo
    
    * actually go to /app/events
    
    * add CalendarSelector component to request page add some todos

---
+++ src/routes/+page.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { goto } from "$app/navigation";
+  import { calendars, identity, access } from "$lib/api";
+
+  let loading = $state("");
+  let loaded = $state(false);
+  const targetLoadingString = ">₍^. .^₎<";
+  let interval: ReturnType<typeof setInterval>;
+
+  // TODO: Run check access once we have successfully subscribed to channels (currently in +layout.svelte)
+  $effect(() => {
+    if (loaded) {
+      checkAccess();
+    }
+  });
+
+  async function checkAccess() {
+    // TODO: cache active calendar somewhere
+    let activeCalendarId = await calendars.getActiveCalendarId();
+
+    if (activeCalendarId) {
+      // TODO: cache publicKey somewhere
+      let publicKey = await identity.publicKey();
+      let accessStatus = await access.checkStatus(publicKey, activeCalendarId);
+
+      if (accessStatus == "accepted") {
+        // access is accepted, go to events
+        goto("/app/events");
+        return;
+      } else if (accessStatus == "pending") {
+        // access status is pending, go to pending page
+        goto("/request");
+        return;
+      }
+    }
+    goto("/join");
+  }
+
+  onMount(() => {
+    interval = setInterval(() => {
+      if (loading.length < targetLoadingString.length) {
+        loading = targetLoadingString.slice(0, loading.length + 1);
+      } else {
+        clearInterval(interval);
+        loaded = true;
+      }
+    }, 100);
+  });
+</script>
+
+<div class="w-screen h-screen flex items-center justify-center">
+  <span>{loading}</span>
+</div>
