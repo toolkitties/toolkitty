b5dd15500e53255704dbbdfb9a0afc6a69a21eb4
Jack MB <hi@jackmurraybrown.com>
2025-03-07 15:08:31 +0100

    remove test error

--- src/routes/app/events/+page.ts
+++ src/routes/app/events/+page.ts
@@ -1,14 +1,9 @@
 import type { PageLoad } from "./$types";
 import { calendars, events } from "$lib/api";
-import { error } from "@sveltejs/kit";
 
 export const load: PageLoad = async () => {
   const activeCalendarId = await calendars.getActiveCalendarId();
   const eventsList = await events.findMany(activeCalendarId!);
-
-  error(404, {
-    message: 'Not found'
-  });
 
   return { title: "home", activeCalendarId, eventsList };
 };
