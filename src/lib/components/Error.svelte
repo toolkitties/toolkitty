4044743daa831449e1390c8b757f967e648795ec
Jack MB <hi@jackmurraybrown.com>
2025-03-07 15:07:16 +0100

    create error component and add +error.svelte pages

---
+++ src/lib/components/Error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { page } from "$app/state";
+</script>
+
+<div class="flex flex-col items-center justify-center">
+  <span>(*_*)</span>
+  <h1>{page.error?.message}</h1>
+</div>
--- src/routes/app/events/+page.ts
+++ src/routes/app/events/+page.ts
@@ -1,9 +1,14 @@
 import type { PageLoad } from "./$types";
 import { calendars, events } from "$lib/api";
+import { error } from "@sveltejs/kit";
 
 export const load: PageLoad = async () => {
   const activeCalendarId = await calendars.getActiveCalendarId();
   const eventsList = await events.findMany(activeCalendarId!);
+
+  error(404, {
+    message: 'Not found'
+  });
 
   return { title: "home", activeCalendarId, eventsList };
 };
