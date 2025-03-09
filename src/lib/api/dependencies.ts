601d915ccb37cb1ca68d4414e92c74533ced71ec
Sam Andreae <contact@samandreae.com>
2025-03-09 00:27:44 +0000

    Trigger replay when dependency arrives

--- src/lib/api/dependencies.ts
+++ src/lib/api/dependencies.ts
@@ -20,7 +20,7 @@ export async function process(message: ApplicationMessage) {
     data.type == "booking_request_accepted" ||
     data.type == "booking_request_rejected"
   ) {
-    return await onBookingResponse(meta, data.data);
+    return await onBookingResponse(data.data);
   } else if (
     data.type == "calendar_access_accepted" ||
     data.type == "calendar_access_rejected"
@@ -35,84 +35,74 @@ export async function process(message: ApplicationMessage) {
     data.type == "calendar_deleted" ||
     data.type == "page_updated"
   ) {
-    return await onCalendarEdit(meta, data.data);
+    return await onCalendarEdit(data.data);
   } else if (data.type == "space_updated" || data.type == "space_deleted") {
-    return await onSpaceEdit(meta, data.data);
+    return await onSpaceEdit(data.data);
   } else if (
     data.type == "resource_updated" ||
     data.type == "resource_deleted"
   ) {
-    return await onResourceEdit(meta, data.data);
+    return await onResourceEdit(data.data);
   } else if (data.type == "event_updated" || data.type == "event_deleted") {
-    return await onEventEdit(meta, data.data);
+    return await onEventEdit(data.data);
+  } else if (
+    data.type == "calendar_created" ||
+    data.type == "event_created" ||
+    data.type == "resource_created" ||
+    data.type == "space_created" ||
+    data.type == "calendar_access_requested" ||
+    data.type == "booking_requested"
+  ) {
+    const topic = new TopicFactory(meta.stream.id);
+    await topics.replay(topic.calendar());
   }
 }
 
 async function onBookingResponse(
-  meta: StreamMessageMeta,
   data: BookingRequestAccepted["data"] | BookingRequestRejected["data"],
 ) {
   const request = await bookings.findRequest(data.requestId);
   if (!request) {
-    const topic = new TopicFactory(meta.stream.id);
-    await topics.replay(topic.calendar());
     throw new Error("resource request not yet received");
   }
 }
 
 async function onCalendarEdit(
-  meta: StreamMessageMeta,
   data: CalendarUpdated["data"] | CalendarDeleted["data"] | PageUpdated["data"],
 ) {
   let calendar = await calendars.findOne(data.id);
   if (!calendar) {
-    const topic = new TopicFactory(meta.stream.id);
-    await topics.replay(topic.calendar());
     throw new Error("calendar not yet received");
   }
 }
 
-async function onSpaceEdit(
-  meta: StreamMessageMeta,
-  data: SpaceUpdated["data"] | SpaceDeleted["data"],
-) {
+async function onSpaceEdit(data: SpaceUpdated["data"] | SpaceDeleted["data"]) {
   let space = await spaces.findById(data.id);
   if (!space) {
-    const topic = new TopicFactory(meta.stream.id);
-    await topics.replay(topic.calendar());
     throw new Error("space not yet received");
   }
 }
 
 async function onResourceEdit(
-  meta: StreamMessageMeta,
   data: ResourceUpdated["data"] | ResourceDeleted["data"],
 ) {
   let resource = await resources.findById(data.id);
   if (!resource) {
-    const topic = new TopicFactory(meta.stream.id);
-    await topics.replay(topic.calendar());
     throw new Error("resource not yet received");
   }
 }
 
-async function onEventEdit(
-  meta: StreamMessageMeta,
-  data: EventUpdated["data"] | EventDeleted["data"],
-) {
+async function onEventEdit(data: EventUpdated["data"] | EventDeleted["data"]) {
   let event = await events.findById(data.id);
   if (!event) {
-    const topic = new TopicFactory(meta.stream.id);
-    await topics.replay(topic.calendar());
     throw new Error("event not yet received");
   }
 }
 
 async function onUserProfileUpdated(meta: StreamMessageMeta) {
-  const user = await users.get(meta.stream.id, meta.author);
+  const calendarId = meta.stream.id;
+  const user = await users.get(calendarId, meta.author);
   if (!user) {
-    const topic = new TopicFactory(meta.stream.id);
-    await topics.replay(topic.calendar());
     throw new Error("user not yet received");
   }
 }
@@ -121,19 +111,17 @@ async function onUserRoleAssigned(
   meta: StreamMessageMeta,
   data: UserRoleAssigned["data"],
 ) {
-  const user = await users.get(meta.stream.id, data.publicKey);
+  const calendarId = meta.stream.id;
+  const user = await users.get(calendarId, data.publicKey);
   if (!user) {
-    const topic = new TopicFactory(meta.stream.id);
-    await topics.replay(topic.calendar());
     throw new Error("user not yet received");
   }
 }
 
 async function onCalendarAccessResponse(meta: StreamMessageMeta) {
-  const calendar = await calendars.findOne(meta.stream.id);
+  const calendarId = meta.stream.id;
+  const calendar = await calendars.findOne(calendarId);
   if (!calendar) {
-    const topic = new TopicFactory(meta.stream.id);
-    await topics.replay(topic.calendar());
     throw new Error("calendar not yet received");
   }
 
