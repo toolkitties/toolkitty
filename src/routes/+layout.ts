fab0adbd555b008928eaad97288971b38dd591bd
Jack MB <hi@jackmurraybrown.com>
2025-03-12 15:01:42 +0100

    add todo

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -8,6 +8,7 @@ import type { LayoutLoad } from "./$types";
 import { calendars, identity } from "$lib/api";
 
 export const load: LayoutLoad = async () => {
+  // TODO: move these to reactive global store
   const activeCalendarId = await calendars.getActiveCalendarId();
   const publicKey = await identity.publicKey();
 
