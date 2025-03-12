be7092d22a113e5678eb8cf7eb2693ff3d3ece64
Jack MB <hi@jackmurraybrown.com>
2025-03-12 15:33:36 +0100

    cache public key in the frontend db

--- src/lib/api/identity.ts
+++ src/lib/api/identity.ts
@@ -1,6 +1,27 @@
 import { invoke } from "@tauri-apps/api/core";
+import { db } from "$lib/db";
 
 export async function publicKey(): Promise<PublicKey> {
-  // @TODO: Would be good to cache this somewhere probably.
-  return await invoke("public_key");
+  try {
+    const cachedPublicKey = await db.settings
+      .get("publicKey")
+      .then((publicKey) => publicKey?.value);
+    if (cachedPublicKey) {
+      return cachedPublicKey;
+    }
+
+    const newPublicKey: PublicKey = await invoke("public_key");
+    if (!newPublicKey) {
+      throw new Error("Failed to retrieve new public key from backend");
+    }
+
+    await db.settings.put({
+      name: "publicKey",
+      value: newPublicKey,
+    });
+
+    return newPublicKey;
+  } catch (error) {
+    throw new Error(`Failed to retrieve public key: ${error}`);
+  }
 }
