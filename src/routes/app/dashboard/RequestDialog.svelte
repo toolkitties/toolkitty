5782c8382c0fc0a047c74967dc83dd4e30e31c18
Jack MB <hi@jackmurraybrown.com>
2025-03-17 13:54:01 +0100

    add enriched data to requests and toasts

--- src/routes/app/dashboard/RequestDialog.svelte
+++ src/routes/app/dashboard/RequestDialog.svelte
@@ -2,14 +2,14 @@
   import BookingRequestDialog from "$lib/components/dialog/BookingRequestDialog.svelte";
   import * as AlertDialog from "$lib/components/dialog/index";
 
-  let { request }: { request: BookingRequest } = $props();
+  let { request }: { request: BookingRequestEnriched } = $props();
 
   let open = $state(false);
 </script>
 
 <AlertDialog.Root bind:open>
-  <AlertDialog.Trigger class="button">
-    <p>{request.id}</p>
+  <AlertDialog.Trigger class="button flex justify-between w-full text-left">
+    <p>{request.resource?.name}</p>
     <span>Pending</span>
     <BookingRequestDialog {request} bind:open />
   </AlertDialog.Trigger>
