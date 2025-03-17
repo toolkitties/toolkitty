99c131a45ca97517feafb2363a6af6017a32c6fb
Jack MB <hi@jackmurraybrown.com>
2025-03-17 14:56:26 +0100

    distinguish between space and resource requests in requests on events page

--- src/routes/app/events/[id]/+page.svelte
+++ src/routes/app/events/[id]/+page.svelte
@@ -5,6 +5,8 @@
   import { bookings } from "$lib/api";
   import Links from "$lib/components/Links.svelte";
   import Date from "$lib/components/Date.svelte";
+  import FootstepsIcon from "$lib/components/icons/FootstepsIcon.svelte";
+  import ChestIcon from "$lib/components/icons/ChestIcon.svelte";
 
   let { data }: PageProps = $props();
 
@@ -33,18 +35,32 @@
     {/if}
   </div>
 
-  {#if data.userRole === "admin" && $upcomingBookings?.length > 0}
+  {#if (data.userRole === "admin" || data.publicKey == data.event.ownerId) && $upcomingBookings?.length > 0}
     <section>
       <h3>Requests</h3>
       {#each $upcomingBookings as booking (booking.id)}
         <div
           class="border border-black p-2 flex justify-between w-full text-left"
         >
-          <span
-            >{booking.resource?.name
-              ? booking.resource?.name
-              : "No name yet"}</span
-          >
+          {#if booking.resourceType === "resource"}
+            <div class="flex gap-2 items-center">
+              <ChestIcon size={22} />
+              <span
+                >{booking.resource?.name
+                  ? booking.resource?.name
+                  : "No name yet"}
+              </span>
+            </div>
+          {:else}
+            <div class="flex gap-2 items-center">
+              <FootstepsIcon size={22} />
+              <span
+                >{booking.space?.name
+                  ? booking.space?.name
+                  : "No name yet"}</span
+              >
+            </div>
+          {/if}
           <span>{booking.status}</span>
         </div>
       {/each}
