f5bed89857f4643447cf233f310e70cc2efbd589
Jack MB <hi@jackmurraybrown.com>
2025-03-18 11:25:22 +0100

    remove comments

--- src/routes/(unauthed)/request/+page.ts
+++ src/routes/(unauthed)/request/+page.ts
@@ -3,10 +3,6 @@ import { access, calendars, identity } from "$lib/api";
 import { goto } from "$app/navigation";
 
 export const load: PageLoad = async () => {
-
-  // Invalidate the layout data so we get latest active calendar
-  // TODO: move active calendar to reactive object so we don't need to invalidate the layout data
-
   const myPublicKey = await identity.publicKey();
   const activeCalendarId = await calendars.getActiveCalendarId();
   const accessStatus = await access.checkStatus(myPublicKey, activeCalendarId!);
