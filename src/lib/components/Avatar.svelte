29a8d2a90ae6ddc91efd12805abc29ae0d429755
Jack MB <hi@jackmurraybrown.com>
2025-03-19 18:11:17 +0000

    update var name

--- src/lib/components/Avatar.svelte
+++ src/lib/components/Avatar.svelte
@@ -1,21 +1,23 @@
 <script lang="ts">
   type Health = "sad" | "ok" | "happy";
 
-  let avatarHealth: Health = "ok";
+  let kittyHealth: Health = $state("ok");
+
+  // TODO: get network status from backend and update avatar health state
 </script>
 
 <div class="w-10 h-full mx-auto">
-  {#if avatarHealth === "sad"}
+  {#if kittyHealth === "sad"}
     <img
       src="/images/toolkitty-avatar-sad.png"
       alt="kitten sleeping next to toolkit box"
     />
-  {:else if avatarHealth === "ok"}
+  {:else if kittyHealth === "ok"}
     <img
       src="/images/toolkitty-avatar-ok.png"
       alt="kitten holding toolkit box"
     />
-  {:else if avatarHealth === "happy"}
+  {:else if kittyHealth === "happy"}
     <img
       src="/images/toolkitty-avatar-happy.png"
       alt="happy kitten sitting inside toolkit box"
