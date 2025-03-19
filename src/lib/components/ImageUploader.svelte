e37d78fbe5a3f0d6b244871d551658f777be135c
adz <x12@adz.garden>
2025-03-19 23:14:07 +0100

    Introduce ImageUploader form component

--- src/lib/components/ImageUploader.svelte
+++ src/lib/components/ImageUploader.svelte
@@ -1,13 +1,35 @@
 <script lang="ts">
+    import { uploadFile } from "$lib/api/blobs";
+
   let {
     images = $bindable(),
   }: {
-    images: string[];
+    images: Image[];
   } = $props();
 
-  images.push('1234');
+  async function selectImage(event: Event) {
+    // FIXME(adz): Note sure why we need to do this as this is strictly
+    // speaking not a submit event, but without it it causes superform to
+    // submit the whole thing.
+    event.preventDefault();
+
+    const image = await uploadFile();
+    images = [...images, image];
+  }
+
+  function removeImage(event: Event, index: number) {
+    event.preventDefault();
+    images = images.filter((_, i) => index != i);
+  }
 </script>
 
-{#each images as image (image)}
-  {image}
-{/each}
+<div class="grid grid-cols-3 gap-4">
+  {#each images as image, index (`${image}${index}`)}
+    <div class="border-2 rounded-lg border-gray-200">
+      <img src={`blobstore://${image}`} alt={image} />
+      <button onclick={(event) => removeImage(event, index)}>Remove</button>
+    </div>
+  {/each}
+</div>
+
+<button onclick={(event) => selectImage(event)}>Upload image</button>
