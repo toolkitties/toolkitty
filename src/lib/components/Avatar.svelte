d631067bb5441ab10f897c6c1914aa42e76e5a66
Jack MB <hi@jackmurraybrown.com>
2025-03-19 18:09:25 +0000

    add alt text for avatar

--- src/lib/components/Avatar.svelte
+++ src/lib/components/Avatar.svelte
@@ -6,10 +6,19 @@
 
 <div class="w-10 h-full mx-auto">
   {#if avatarHealth === "sad"}
-    <img src="/images/toolkitty-avatar-sad.png" alt="" />
+    <img
+      src="/images/toolkitty-avatar-sad.png"
+      alt="kitten sleeping next to toolkit box"
+    />
   {:else if avatarHealth === "ok"}
-    <img src="/images/toolkitty-avatar-ok.png" alt="" />
+    <img
+      src="/images/toolkitty-avatar-ok.png"
+      alt="kitten holding toolkit box"
+    />
   {:else if avatarHealth === "happy"}
-    <img src="/images/toolkitty-avatar-happy.png" alt="" />
+    <img
+      src="/images/toolkitty-avatar-happy.png"
+      alt="happy kitten sitting inside toolkit box"
+    />
   {/if}
 </div>
