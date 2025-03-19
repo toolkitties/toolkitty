6f9f3ad872738483bf0150bd6d4a50dd236951b3
adz <x12@adz.garden>
2025-03-19 23:22:08 +0100

    Add a comment on image de-duplication

--- src/lib/components/ImageUploader.svelte
+++ src/lib/components/ImageUploader.svelte
@@ -24,6 +24,11 @@
 </script>
 
 <div class="grid grid-cols-3 gap-4">
+  <!--
+    Same images are de-duplicated on the backend and result in the same hash.
+    We still want to show the same image multiple times though if the user
+    wants that, this is why we're using the index as part of the key.
+  -->
   {#each images as image, index (`${image}${index}`)}
     <div class="border-2 rounded-lg border-gray-200">
       <img src={`blobstore://${image}`} alt={image} />
