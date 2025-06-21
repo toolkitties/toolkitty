e791a586de8020c3ad97df88845747e47e4199f1
mikozet <mikozet2@gmail.com>
2025-06-21 12:03:23 +0200

    Update Header component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -9,28 +9,40 @@
  - Avatar
   -->
 
-<script>
+<script lang="ts">
   import Back from "./Back.svelte";
+  import type { PageProps } from "./$types";
   import { calendars } from "$lib/api";
   import Avatar from "./Avatar.svelte";
   import QuestionMark from "./QuestionMark.svelte";
 
   // TODO: change or remove default title once we have a name.
-  let { title = "(^._.^)ﾉ" } = $props();
+  let { title = "(^._.^)ﾉ", data } = $props();
 
   const activeCalendarName = calendars.getActiveCalendar;
 </script>
 
 <div
-  class="p-3 right-2 w-full bg-grey-very-light space-y-1.5 border-b border-black"
+  class="p-3 right-2 w-full bg-grey-very-light space-y-1.5 border-b border-black header"
 >
   <div class="flex justify-between border border-black rounded-full px-3">
     <span>{$activeCalendarName}</span>
     <span>{title}</span>
   </div>
   <div class="grid grid-cols-3 items-center h-10">
-    <Back />
+    <div class="flex flex-row items-center gap-2">
+      <Back />
+      {#if data && data.userRole === "admin"}
+        <a
+          class="button-green button inline-block text-center"
+          href="/app/calendars/edit"
+        >
+          <span>edit</span>
+        </a>
+      {/if}
+    </div>
     <Avatar />
+    <!-- TODO add logic for question mark -->
     <QuestionMark />
   </div>
 </div>
