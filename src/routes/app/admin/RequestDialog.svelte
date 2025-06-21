920981bb89cb4070cb64dd23ebaabdf76e596906
mikozet <mikozet2@gmail.com>
2025-06-21 12:04:50 +0200

    Update RequestDialog component

--- src/routes/app/admin/RequestDialog.svelte
+++ src/routes/app/admin/RequestDialog.svelte
@@ -7,19 +7,30 @@
   }: { data: User | AccessRequest; publicKey?: PublicKey } = $props();
   let open = $state(false);
   let isAccessRequest = (data as AccessRequest).from !== undefined;
+  let role = (data as User).role;
 </script>
 
 <AlertDialog.Root bind:open>
-  <AlertDialog.Trigger class="button flex justify-between w-full text-left">
-    <span>
+  <AlertDialog.Trigger
+    class="w-full bg-white border border-black rounded flex items-center justify-between p-2 px-3"
+  >
+    <span class="flex items-center gap-2 text-[17px]">
       {data.name ? data.name : "no name yet"}
       {!isAccessRequest && (data as User).publicKey === publicKey
         ? "(you)"
         : ""}
     </span>
-    <span>
+    <button
+      class="button-small {isAccessRequest
+        ? 'button-light-yellow'
+        : role === 'admin'
+          ? 'button-green'
+          : role === 'organiser'
+            ? 'button-light-pink'
+            : 'button-light-blue'}"
+    >
       {isAccessRequest ? "pending" : (data as User).role}
-    </span>
+    </button>
     <AccessRoleDialog {data} bind:open />
   </AlertDialog.Trigger>
 </AlertDialog.Root>
