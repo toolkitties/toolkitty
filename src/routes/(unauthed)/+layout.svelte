6714ba2522d3b61cc0f7181686f779e1f03864ff
mikozet <mikozet2@gmail.com>
2025-06-25 18:53:05 +0200

    Add padding top and bottom to unauthed version of layout

--- src/routes/(unauthed)/+layout.svelte
+++ src/routes/(unauthed)/+layout.svelte
@@ -5,7 +5,7 @@
   let { data, children }: LayoutProps = $props();
 </script>
 
-<main class="pt-8 flex flex-col gap-2 h-dvh grow overflow-auto px-3.5">
+<main class="pt-8 flex flex-col gap-2 h-dvh grow overflow-auto px-3.5 py-3.5">
   <!-- TODO: improve conditional for showing back button. -->
   {#if data.activeCalendarId}
     <div class="fixed top-2 left-2">
